b5c2ff2f4533545d85a72734694d5cc2c76c1459
vommidapuchinni <161326469+vommidapuchinni@users.noreply.github.com>
2024-04-26 12:02:53 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
   count = 1
-  key_name      = "terraform-key"
+  key_name      = "terraform-1"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
